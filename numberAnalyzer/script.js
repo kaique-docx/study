edcc8aa77b4df91da258a016a021bbc2daccea1b
kaique-docx <kaiquesouzza0105@gmail.com>
2026-04-23 22:37:33 -0300

    Página de Visualização

--- numberAnalyzer/script.js
+++ numberAnalyzer/script.js
@@ -1,10 +1,9 @@
-var num = document.getElementById("number").value
-var res = document.getElementById("res")
-var parity = "even"
-var natural = "positive"
-
 function analyseNumber(num){
+    let num = document.getElementById("number").value
+    let res = document.getElementById("res")
+
     //Positive, Negative or Zero
+    let natural = "positive"
     if (num > 0){
         natural = "positive"
     } else if (num < 0){
@@ -14,15 +13,17 @@ function analyseNumber(num){
     }
 
     //Even or Odd
+    let parity = "even"
     if (num % 2 == 0) {
         parity = "Even"
     } else {
         parity = "Odd"
     }
     
+    //Number List
     res.innerHTML += "The number is " + natural + " and " + parity + "<br> Number Sequence List: <br>"
 
-    for(i = 1; i <= num; i++){
+    for(let i = 1; i <= num; i++){
         res.innerHTML += i + ", ";
     }
 }
